388667a8d2ade8be8631ea2346e6e512aead00c8
Luis Fullview <luis@fullview.io>
2024-12-02 13:28:32 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FullviewSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "FullviewSDK",
+            targets: [
+                "FullviewSDK"
+            ]
+        )
+    ],
+    dependencies: [ ],
+    targets: [
+        .binaryTarget(
+            name: "FullviewSDK",
+            url:
+                "https://github.com/fullviewdev/fullview-sdk-ios/releases/download/0.9.0/FullviewSDK.xcframework.zip",
+            checksum: "8687f2b49932512e5c91e4c6dffd20e21d559b56fa9aacf68d668fce96daeec9"
+        )
+    ]
+)
